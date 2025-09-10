2a214217d705bcb94734a9ea81b0f1a1dcad0f19
Shidochiri <ruthwk2@gmail.com>
2025-09-10 13:06:37 +0300

    Add basic_operations.py with math functions

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum = 10 + 5
+difference = 10 - 5
+product = 10 * 5
+print("Addition of", number1, "and", number2, "is", sum)
+print("Subtraction of", number1, "and", number2, "is", difference)
+print("Multiplication of", number1, "and", number2, "is", product)
